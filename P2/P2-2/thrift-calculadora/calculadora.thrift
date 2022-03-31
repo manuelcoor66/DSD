dd2d42df654a1408fd4b4593c00b0d6a5a61bb18
Manuel Contreras Orge <91686722+manuelcoor66@users.noreply.github.com>
2022-04-01 01:30:23 +0200

    Add files via upload

---
+++ P2/P2-2/thrift-calculadora/calculadora.thrift
@@ -0,0 +1,12 @@
+service Calculadora{
+   void ping(),
+   i32 suma(1:i32 num1, 2:i32 num2),
+   i32 resta(1:i32 num1, 2:i32 num2),
+   i32 multiplicacion(1:i32 num1, 2:i32 num2),
+   double division(1:double num1, 2:double num2),
+   i32 modulo(1:i32 num1, 2:i32 num2),
+   i32 elevar(1:i32 num1, 2:i32 num2),
+   double raiz_cuadrada(1:i32 num1),
+   double logaritmo(1:i32 num1),
+   i32 valor_absoluto(1:i32 num1),
+}
